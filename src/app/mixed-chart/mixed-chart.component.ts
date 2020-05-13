e2a74d7d16fbae51ea83228b438fb99555487d4d
kudarpakeg <kudarpakeg@gmail.com>
2020-05-14 00:54:57 +0530

    multiple data with same chart implemented.

--- src/app/mixed-chart/mixed-chart.component.ts
+++ src/app/mixed-chart/mixed-chart.component.ts
@@ -27,50 +27,84 @@ export class MixedChartComponent implements OnInit {
 
   @ViewChildren('pr_chart', { read: ElementRef }) chartElementRefs: QueryList<ElementRef>;
 
+  // chartData: Chart.ChartData[] = [
+  //   {
+  //     labels: ['1500', '1600', '1700', '1750', '1800', '1850', '1900', '1950', '1999', '2050'],
+  //     datasets: [{
+  //       data: [86, 378, 106, 306, 507, 111, 133, 221, 783, 5000],
+  //       borderColor: 'red',
+  //       fill: false
+  //     }]
+  //   },
+  //   {
+  //     labels: ['1500', '1600', '1700', '1750', '1800', '1850', '1900', '1950', '1999', '2050'],
+  //     datasets: [{
+  //       data: [86, 378, 106, 306, 507, 111, 133, 221, 783, 5000].reverse(),
+  //       borderColor: 'blue',
+  //       fill: false
+  //     }]
+  //   },
+  //   {
+  //     labels: ['500', '1600', '700', '750', '100', '1850', '1900', '950', '999', '2050'],
+  //     datasets: [{
+  //       data: [86, 378, 106, 306, 507, 111, 133, 221, 783, 5000].reverse(),
+  //       borderColor: 'blue',
+  //       fill: false
+  //     }]
+  //   },
+  //   // {  //Not working for Bar chart.
+  //   //   labels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'],
+  //   //   datasets: [{
+  //   //     data: [{data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
+  //   //     {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}],
+  //   //     borderColor: 'blue',
+  //   //     fill: false
+  //   //   }],
+  //   //   type:'bar',
+  //   // },
+  //   {
+  //     labels : ['Sales Q1', 'Sales Q2', 'Sales Q3', 'Sales Q4'],
+  //   datasets: [{
+  //      data:[120, 150, 180, 90].reverse()
+  //     }],
+  //   type: 'pie',
+  //   }
+  // ];
+
+
   chartData: Chart.ChartData[] = [
     {
       labels: ['1500', '1600', '1700', '1750', '1800', '1850', '1900', '1950', '1999', '2050'],
       datasets: [{
         data: [86, 378, 106, 306, 507, 111, 133, 221, 783, 5000],
         borderColor: 'red',
-        fill: false
+        fill: true
       }]
     },
     {
       labels: ['1500', '1600', '1700', '1750', '1800', '1850', '1900', '1950', '1999', '2050'],
       datasets: [{
-        data: [86, 378, 106, 306, 507, 111, 133, 221, 783, 5000].reverse(),
+        data: [3186, 378, 106, 306, 507, 111, 133, 221, 783, 5000].reverse(),
         borderColor: 'blue',
-        fill: false
-      }]
-    },
-    {
-      labels: ['500', '1600', '700', '750', '100', '1850', '1900', '950', '999', '2050'],
-      datasets: [{
-        data: [86, 378, 106, 306, 507, 111, 133, 221, 783, 5000].reverse(),
-        borderColor: 'blue',
-        fill: false
-      }]
-    },
-    // {  //Not working for Bar chart.
-    //   labels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'],
-    //   datasets: [{
-    //     data: [{data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-    //     {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}],
-    //     borderColor: 'blue',
-    //     fill: false
-    //   }],
-    //   type:'bar',
-    // },
-    {
-      labels : ['Sales Q1', 'Sales Q2', 'Sales Q3', 'Sales Q4'],
-    datasets: [{
-       data:[120, 150, 180, 90].reverse()
-      }],
-    type: 'pie',
+        fill: true
+      },
+      {
+        data: [86, 378, 4106, 306, 507, 111, 1233, 221, 783, 5000],
+        borderColor: 'orange',
+        fill: true
+      },
+      {
+        data: [378, 106, 306, 507, 1111, 4133, 221, 783, 4000,4500],
+        borderColor: 'green',
+        fill: true
+      },{
+        data: [378, 106, 306, 507, 1111, 133, 221, 783, 4000,4500].reverse(),
+        borderColor: 'read',
+        fill: true
+      }
+      ]
     }
   ];
-
   charts: Chart[] = [];
 
   ngAfterViewInit() {
